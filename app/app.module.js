722a5f22effbe9203b0ce2fe6744bca0d2a3c5cc
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-02-11 09:07:39 +0530

    3rd commit

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,9 @@
 
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
-  // ...which depends on the `phoneList` module
+  'ngAnimate',
+  'ngRoute',
+  'core',
+  'phoneDetail',
   'phoneList'
 ]);
--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,18 +5,10 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
-    }
+    controller: ['Phone',
+      function PhoneListController(Phone) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+      }
+    ]
   });
